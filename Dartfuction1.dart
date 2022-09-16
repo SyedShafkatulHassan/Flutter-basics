bd8101291e020f9d7a6fbd704ecb47dc08e4ca7a
SyedShafkatulHassan <87982712+SyedShafkatulHassan@users.noreply.github.com>
2022-09-16 14:45:02 +0600

    Create Dartfuction1.dart

---
+++ Dartfuction1.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+String getstring() {
+  return "hello";
+}
+
+int getint() {
+  return 10;
+}
+
+void main() {
+  String get1 = getstring();
+  int get2 = getint();
+  print(get1);
+  print(get2);
+}
