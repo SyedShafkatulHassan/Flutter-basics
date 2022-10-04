ecf5cb1819fff2c849dd1ad982e5a56f0715cdab
SyedShafkatulHassan <87982712+SyedShafkatulHassan@users.noreply.github.com>
2022-10-04 13:34:19 +0600

    Update RaisedButton.dart

--- RaisedButton.dart
+++ RaisedButton.dart
@@ -5,17 +5,22 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print("hello");
     return MaterialApp(
       theme: new ThemeData(
           scaffoldBackgroundColor: Color.fromARGB(255, 62, 28, 184)),
       home: Scaffold(
         body: Center(
-           child: RaisedButton(
-            onPressed: () {},
-            child: Text('Click Here',style: TextStyle(fontSize: 30.01),),
-            color: Color.fromARGB(255, 252, 4, 24),
-          )
-        ),
+            child: RaisedButton(
+          onPressed: () {
+            print("YouClicked me");
+          },
+          child: Text(
+            'Click Here',
+            style: TextStyle(fontSize: 30.01),
+          ),
+          color: Color.fromARGB(255, 252, 4, 24),
+        )),
       ),
     );
   }
